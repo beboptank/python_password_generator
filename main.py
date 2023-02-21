808dc1a14c47c29a88e00a453b68080c94d448b0
Seth Christmus <seth.christmus@fdmgroup.com>
2023-02-21 15:45:55 -0500

    added code to welcome users and then gather user input on number of letters, symbols, and numbers

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+
+print("Welcome to PassBuddy, a free-to-use password generator.")
+
+num_of_letters = int(input("How many letters would you like in your password?\n"))
+num_of_symbols = int(input("How many symbols would you like?\n"))
+num_of_numbers = int(input("How many numbers would you like?\n"))
